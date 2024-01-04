105ebf5e3c7211001e0c89dc7bd0299950721550
Abhishekgaikwad017 <144792748+Abhishekgaikwad017@users.noreply.github.com>
2024-01-04 15:16:45 +0530

    Update Connect.cs

--- Day9/DAL/Connect.cs
+++ Day9/DAL/Connect.cs
@@ -8,7 +8,7 @@ public class Connect
     public static MySqlConnection getConnection(){
         if(conn==null){
             // conn = new MySqlConnection();
-            String connstr= "server=192.168.10.150;port=3306;user=dac9;password=welcome;database=dac9";
+            String connstr= "server=192.168.10.150;port=3306;user=dac23;password=welcome;database=dac23";
             conn = new MySqlConnection(connstr);
         }
         return conn;
